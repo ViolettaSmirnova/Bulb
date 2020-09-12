57e9012195668ebbed74c4b5fb3870f7d6a8bd8e
ViolettaSmirnova <smirnova.violett@mail.ru>
2020-09-12 22:02:48 +0300

    Создание

---
+++ bulb.js
@@ -0,0 +1,14 @@
+"use strict";
+let bulb = document.querySelector(".bulb");
+let input = document.querySelector("input");
+
+  document.querySelector(".turnOn").onclick = function fun1() {
+  bulb.setAttribute("src", "bulb2.png");
+  input.setAttribute("style", "background-color:lightgray");
+  fun1();
+};
+  document.querySelector(".turnOff").onclick = function fun2() {
+  bulb.setAttribute("src", "bulb1.png");
+  input.removeAttribute("style");
+  fun2();
+};
---
+++ bulb.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="bulb.css">
+</head>
+<body>
+  <div class = "container">
+    <div><img class = "bulb" src="bulb1.png" width = 14% alt="lamp idle"></div>
+    <!--<div><img class = "active" src="bulb2.png" alt="lamp active"></div>-->
+  </div>
+  <div class = "block">
+    <form>
+     <div><input type="button" class = "turnOn"  value="ВКЛ">
+     <input type="button" class = "turnOff" value="ВЫКЛ"></div>
+     
+    </form>
+  </div>
+  <script src = "bulb.js"></script>
+</body>
+</html>
